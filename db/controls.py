e90cb0c2d6549a700b0ae4787db878840284ebca
djx <cloveses@126.com>
2017-05-05 22:25:39 +0800

    Update controls.py

--- db/controls.py
+++ db/controls.py
@@ -13,6 +13,7 @@ def get_session():
 def user_exist(name):
     sess = get_session()
     q = sess.query(User).filter_by(name=name).first()
+    sess.close()
     if q:
         return True
 
@@ -23,6 +24,7 @@ def add_user(name,passwd):
         u = User(name=name,passwd=passwd)
         sess.add(u)
         sess.commit()
+        sess.close()
         return u
 
 def get_user(name,passwd):
@@ -31,20 +33,25 @@ def get_user(name,passwd):
         sess = get_session()
         q = sess.query(User).filter_by(name=name)
         res = q.filter_by(passwd=passwd).first()
+        sess.close()
         return res
 
 def del_user(name):
     if name:
         sess = get_session()
         sess.query(User).filter_by(name=name).delete()
+        sess.commit()
+        sess.close()
 
 def proj_exist(name,url):
     sess = get_session()
     q = sess.query(ProjectName).filter_by(name=name).first()
     if q:
+        sess.close()
         return True
     q = sess.query(ProjectName).filter_by(url=url).first()
     if q:
+        sess.close()
         return True
 
 def add_proname(name,url,introduce):
@@ -55,6 +62,7 @@ def add_proname(name,url,introduce):
         sess = get_session()
         sess.add(p)
         sess.commit()
+        sess.close()
 
 def chn_status(name):
     if name:
@@ -63,11 +71,14 @@ def chn_status(name):
         if p:
             p.status = not p.status
             sess.commit()
+            sess.close()
 
 def del_pro(name):
     if name:
         sess = get_session()
         sess.query(ProjectName).filter_by(name=name).delete()
+        sess.commit()
+        sess.close()
 
 def get_pro(name):
     if name:
@@ -86,6 +97,7 @@ def get_info_url(url):
     if url:
         sess = get_session()
         proj = sess.query(ProjectName).filter_by(url=url).first()
+        sess.close()
         if proj:
             return proj.name,proj.introduce
     return '',''
